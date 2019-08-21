245eec2e79af22eaea4e6623b8e0ed043ebd3d0b
Koji Wakamiya <koji.wakamiya@gmail.com>
2019-08-21 09:31:25 +0900

    Setup DataBinding

--- app/src/main/java/com/dr1009/app/coroutineexceptiondemo/ui/main/MainFragment.kt
+++ app/src/main/java/com/dr1009/app/coroutineexceptiondemo/ui/main/MainFragment.kt
@@ -1,17 +1,22 @@
 package com.dr1009.app.coroutineexceptiondemo.ui.main
 
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.dr1009.app.coroutineexceptiondemo.R
+import com.dr1009.app.coroutineexceptiondemo.databinding.MainFragmentBinding
 
 class MainFragment : Fragment(R.layout.main_fragment) {
 
     private val viewModel by viewModels<MainViewModel>()
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = MainFragmentBinding.bind(view)
+
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
     }
 
 }
