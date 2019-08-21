815ca2812a2260a08d54727e5b4dc1e6b104a885
Koji Wakamiya <koji.wakamiya@gmail.com>
2019-08-21 09:06:49 +0900

    Use fragment-ktx

--- app/src/main/java/com/dr1009/app/coroutineexceptiondemo/MainActivity.kt
+++ app/src/main/java/com/dr1009/app/coroutineexceptiondemo/MainActivity.kt
@@ -1,19 +1,5 @@
 package com.dr1009.app.coroutineexceptiondemo
 
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import com.dr1009.app.coroutineexceptiondemo.ui.main.MainFragment
 
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
-    }
-
-}
+class MainActivity : AppCompatActivity(R.layout.main_activity)
--- app/src/main/java/com/dr1009/app/coroutineexceptiondemo/ui/main/MainFragment.kt
+++ app/src/main/java/com/dr1009/app/coroutineexceptiondemo/ui/main/MainFragment.kt
@@ -1,29 +1,16 @@
 package com.dr1009.app.coroutineexceptiondemo.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.dr1009.app.coroutineexceptiondemo.R
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(R.layout.main_fragment) {
 
-    companion object {
-        fun newInstance() = MainFragment()
-    }
-
-    private lateinit var viewModel: MainViewModel
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
-    }
+    private val viewModel by viewModels<MainViewModel>()
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
